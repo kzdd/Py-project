3f5299927ead309d672b3270c2ec4c88c7a16515
Kai Z <kaiz@Whose-Power-Reactor.local>
2017-12-28 23:58:48 -0800

    add logging feature

--- tests/linux/tc1.py
+++ tests/linux/tc1.py
@@ -1,16 +1,24 @@
 import pprint
+import logging
+import os
+
+LOG = logging.getLogger(os.path.basename(__file__))
 
 class tc1:
     def __init__(self):
+        LOG.info("In the constructor")
         pprint.pprint("In the constructor")
 
     def setup(self):
+        LOG.info("In the setup")
         pprint.pprint("In the setup")
 
     def test(self):
+        LOG.info("In test")
         pprint.pprint("In test")
 
     def teardown(self):
+        LOG.info("In the teardown")
         pprint.pprint("In the teardown")
 
 
